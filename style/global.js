4d4892f0ab4e5cf090334795c65730c201a2ca63
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-12 08:59:52 -0600

    validacion al Login

--- style/global.js
+++ style/global.js
@@ -3,39 +3,55 @@ import {StyleSheet} from 'react-native';
 const globalStyles = StyleSheet.create({
   contenedor: {
     // flex: 1,
+   
   },
-  contenido:{
-    flexDirection:'column',
-    justifyContent:'center',
-    marginHorizontal:'4%',
-    flex:1
+  contenido: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    
+    flex: 1,
+     backgroundColor: '#E4E8EA',
   },
-  titulo:{
-    textAlign:'center',
-    marginBottom:8,
-    fontSize:32,
-    fontWeight:'bold',
-  },
-  boton:{
-    marginTop:15,
-    backgroundColor:'#28303B',
-  },
-  botontext:{
-    textTransform:'uppercase',
-    fontWeight:'bold',
-    color:'#fff'
-  },
-  enlace:{
-    marginTop:60,
-    textAlign:'center',
-    fontWeight:'bold',
-    fontSize:16,
-    textTransform:'uppercase'
+  contenido2: {
+    backgroundColor: '#fff',
+    paddingHorizontal: '2.5%',
+    marginHorizontal: '3%',
+    paddingVertical: '2.5%',
+    borderRadius: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
 
-  }
-  
-
- 
+    elevation: 3,
+  },
+  titulo: {
+    textAlign: 'center',
+    marginBottom: 8,
+    paddingTop: 8,
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  boton: {
+    marginTop: 15,
+    backgroundColor: '#389BF5',
+  },
+  botontext: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  enlace: {
+    marginTop: 50,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textTransform: 'uppercase',
+    paddingBottom: 5,
+  },
 });
 
 export default globalStyles;
--- views/Login.jsx
+++ views/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState}  from 'react';
 import {StyleSheet, View} from 'react-native';
 import {
   NativeBaseProvider,
@@ -11,24 +11,73 @@ import {
 } from 'native-base';
 import globalStyles from '../style/global';
 import {useNavigation} from '@react-navigation/native';
+//APOLLO
+import {gql, useMutation} from '@apollo/client';
 
+const AUTENTICAR_USUARIO = gql`
+mutation autenticarUsuario($input:AutenticarInput){
+  autenticarUsuario(input: $input){
+    token
+  }
+}
+`
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [mensaje, guardarMensaje] = useState(null);
+
   //funcion para redireccionar
   const navigation = useNavigation();
+
+    //apollo
+  const [autenticarUsuario] = useMutation(AUTENTICAR_USUARIO);
+
+  const handleSubmit= async()=>{
+    if ( email === '' || password === '') {
+      mostrarAlerta();
+      return;
+    }
+
+    try {
+      const {data} = await autenticarUsuario({
+        variables:{
+          input :{
+            email,
+            password
+          }
+        }
+      });
+      console.log(data)
+      
+    } catch (error) {
+      const advertencia = error.message.replace('GraphQL error', '');
+      guardarMensaje(error.message.replace('GraphQL error', ''))
+      Alert.alert('Error', `${mensaje}`, [{text: 'Ok', style: 'cancel'}]);
+      
+    }
+
+  }
+  const mostrarAlerta = () => {
+    Alert.alert('Error', 'Todos los campos son obligatorios', [
+      {text: 'Ok', style: 'cancel'},
+    ]);
+  };
   return (
     <NativeBaseProvider style={[globalStyles.contenedor]}>
       <View style={globalStyles.contenido}>
-        <Heading style={globalStyles.titulo}>Login</Heading>
+        <View style={globalStyles.contenido2}>
+        <Heading style={globalStyles.titulo}>UpTask</Heading>
         <FormControl>
           <Stack space={5}>
             <Stack>
-              <FormControl.Label>Username</FormControl.Label>
+              <FormControl.Label>Email</FormControl.Label>
               <Input
                 variant="underlined"
                 autoComplete="email"
                 p={2}
-                placeholder="Email"
+                 placeholder="Email"
+                onChangeText={texto => setEmail(texto)}
               />
             </Stack>
             <Stack>
@@ -38,12 +87,14 @@ const Login = () => {
                 variant="underlined"
                 p={2}
                 placeholder="contraseña"
+                onChangeText={texto => setPassword(texto)}
               />
             </Stack>
           </Stack>
         </FormControl>
 
-        <Button style={globalStyles.boton} borderRadius="full">
+        <Button  onPress={()=>handleSubmit()}
+        style={globalStyles.boton} borderRadius="full">
           <Text style={globalStyles.botontext}>Iniciar Sesión</Text>
         </Button>
 
@@ -53,6 +104,7 @@ const Login = () => {
         >
           Crear Cuenta
         </Text>
+        </View>
       </View>
     </NativeBaseProvider>
   );
