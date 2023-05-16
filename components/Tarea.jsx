1fbab3961054755e3960b9a24e9b250f5baada52
Diana Rut <93566502+DIANARUTGARCIA@users.noreply.github.com>
2023-05-16 10:11:25 -0600

    cambios antes de hacer otra rama

--- components/Tarea.jsx
+++ components/Tarea.jsx
@@ -21,8 +21,99 @@ const OBTENER_TAREAS = gql`
     }
   }
 `;
+const ACTUALIZAR_TAREA = gql`
+  mutation actualizarTarea($id: ID!, $input: TareaInput, $estado: Boolean) {
+    actualizarTarea(id: $id, input: $input, estado: $estado) {
+      nombre
+      id
+      proyecto
+      estado
+    }
+  }
+`;
+
+const ELIMINAR_TAREA = gql`
+  mutation eliminarTarea($id: ID!) {
+    eliminarTarea(id: $id)
+  }
+`;
 
 const Tarea = () => {
+  const [actualizarTarea] = useMutation(ACTUALIZAR_TAREA);
+  const [eliminarTarea] = useMutation(ELIMINAR_TAREA, {
+    update(cache) {
+      const {obtenerTareas} = cache.readQuery({
+        query: OBTENER_TAREAS,
+        variables: {
+          input: {
+            proyecto: proyectoId,
+          },
+        },
+      });
+
+      cache.writeQuery({
+        query: OBTENER_TAREAS,
+        variables: {
+          input: {
+            proyecto: proyectoId,
+          },
+        },
+        data: {
+          obtenerTareas: obtenerTareas.filter(
+            tareaActual => tareaActual.id !== tarea.id,
+          ),
+        },
+      });
+    },
+  });
+
+  const cambiarEstado = async () => {
+    // obtener el ID de la tarea
+    const {id} = tarea;
+    console.log(!tarea.estado);
+
+    try {
+      const {data} = await actualizarTarea({
+        variables: {
+          id,
+          input: {
+            nombre: tarea.nombre,
+          },
+          estado: !tarea.estado,
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const mostrarEliminar = () => {
+    Alert.alert('Eliminar Tarea', '¿Deseas eliminar esta tarea?', [
+      {
+        text: 'Cancelar',
+        style: 'cancel',
+      },
+      {
+        text: 'Confirmar',
+        onPress: () => eliminarTareaDB(),
+      },
+    ]);
+  };
+  const eliminarTareaDB = async () => {
+    const {id} = tarea;
+
+    try {
+      const {data} = await eliminarTarea({
+        variables: {
+          id,
+        },
+      });
+
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <NativeBaseProvider>
       <View>
--- config/apollo.js
+++ config/apollo.js
@@ -1,25 +1,38 @@
+// import { ApolloClient} from '@apollo/client'
+// import {InMemoryCache} from 'apollo-cache-inmemory'
+// import {createHttpLink} from 'apollo-link-http'
+// import {setContext} from 'apollo-link-context'
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+ 
+// const htttplink = createHttpLink('http://192.168.0.17:4000/')
+
+//  const authLink = setContext(async(_,{headers})=>{
+//    const token = await AsyncStorage.getItem('token');
+//    console.log(token)
+//    return {
+//     headers:{
+//         ...headers,
+//         authorization: token ? `Bearer ${token}`: ''
+//     }
+//    }
+//  })
+
+// const client = new ApolloClient({
+//     cache: new InMemoryCache(),
+//     link: authLink.concat(htttplink)
+// })
+
+
+// export default client;
 import { ApolloClient} from '@apollo/client'
 import {InMemoryCache} from 'apollo-cache-inmemory'
-import {createHttpLink} from 'apollo-link-http'
-import {setContext} from 'apollo-link-context'
-import AsyncStorage from '@react-native-async-storage/async-storage';
- 
-const htttplink = createHttpLink('http://192.168.0.17:4000/')
-
- const authLink = setContext(async(_,{headers})=>{
-   const token = await AsyncStorage.getItem('token');
-   console.log(token)
-   return {
-    headers:{
-        ...headers,
-        authorization: token ? `Bearer ${token}`: ''
-    }
-   }
- })
+import {HttpLink} from 'apollo-link-http'
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: authLink.concat(htttplink)
+    link: new HttpLink({
+        uri:'http://192.168.0.17:4000/'
+    })
 })
 
 
